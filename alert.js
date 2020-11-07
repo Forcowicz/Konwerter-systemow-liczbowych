5f97bfd1d845c19794651b3900f8e8e1eeef5c61
Forcowicz <forcowicz@gmail.com>
2020-11-07 21:37:50 +0100

    Credits introduction

---
+++ alert.js
@@ -0,0 +1,34 @@
+const modal = document.getElementById('modal');
+const btn = document.getElementById('modalBtn');
+const cookie = getCookie("Visited");
+if (cookie === null) {
+    modal.classList.toggle('visitor-alert-hidden');
+    btn.addEventListener('click', () => {
+        modal.classList.toggle('visitor-alert-hidden');
+        document.cookie = `Visited=true;expires=${setExpirationDate()};path=/`;
+    });
+}
+
+function getCookie(name) {
+    const prefix = name + "="; // "Visited="
+    let begin = document.cookie.indexOf("; " + prefix);
+    if(begin === -1) {
+        begin = document.cookie.indexOf(prefix);
+        if(begin != 0) return null;
+    } else {
+        begin += 2;
+        let end = document.cookie.indexOf(";", begin);
+        if (end === -1) {
+            end = document.cookie.length;
+        }
+    }
+    return decodeURI(document.cookie.substring(begin + prefix.length));
+}
+
+function setExpirationDate() {
+    const now = new Date();
+    const time = now.getTime();
+    const expireTime = time + 3000 * 60 * 60 * 1000;
+    now.setTime(expireTime);
+    return now.toUTCString();
+}
--- modal.js
+++ modal.js
@@ -1,34 +1,11 @@
-const modal = document.getElementById('modal');
-const btn = document.getElementById('modalBtn');
-const cookie = getCookie("Visited");
-if (cookie === null) {
-    modal.classList.toggle('visitor-alert-hidden');
-    btn.addEventListener('click', () => {
-        modal.classList.toggle('visitor-alert-hidden');
-        document.cookie = `Visited=true;expires=${setExpirationDate()};path=/`;
-    });
-}
+const creditsModal = document.getElementById('creditsModal');
+const creditsModalBtnOpen = document.getElementById('creditsBtnOpen');
+const creditsModalBtnClose = document.getElementById('creditsBtnClose');
 
-function getCookie(name) {
-    const prefix = name + "="; // "Visited="
-    let begin = document.cookie.indexOf("; " + prefix);
-    if(begin === -1) {
-        begin = document.cookie.indexOf(prefix);
-        if(begin != 0) return null;
-    } else {
-        begin += 2;
-        let end = document.cookie.indexOf(";", begin);
-        if (end === -1) {
-            end = document.cookie.length;
-        }
-    }
-    return decodeURI(document.cookie.substring(begin + prefix.length));
-}
+creditsModalBtnOpen.addEventListener('click', () => {
+    creditsModal.classList.toggle('credits-hidden');
+});
 
-function setExpirationDate() {
-    const now = new Date();
-    const time = now.getTime();
-    const expireTime = time + 3000 * 60 * 60 * 1000;
-    now.setTime(expireTime);
-    return now.toUTCString();
-}
+creditsModalBtnClose.addEventListener('click', () => {
+    creditsModal.classList.toggle('credits-hidden');
+});
