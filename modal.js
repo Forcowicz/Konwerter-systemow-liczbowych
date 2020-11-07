656d589d5030920366dcc8772ecbee522b975edf
Forcowicz <forcowicz@gmail.com>
2020-11-07 12:05:26 +0100

    Updated cookie

--- modal.js
+++ modal.js
@@ -5,7 +5,7 @@ if (cookie === null) {
     modal.classList.toggle('visitor-alert-hidden');
     btn.addEventListener('click', () => {
         modal.classList.toggle('visitor-alert-hidden');
-        document.cookie = "Visited=true";
+        document.cookie = "Visited=true;expires=" + setExpirationDate(); + ";path=/";
     });
 }
 
@@ -23,4 +23,12 @@ function getCookie(name) {
         }
     }
     return decodeURI(document.cookie.substring(begin + prefix.length));
+}
+
+function setExpirationDate() {
+    const now = new Date();
+    const time = now.getTime();
+    const expireTime = time + 3000 * 60 * 60 * 1000;
+    now.setTime(expireTime);
+    return now.toUTCString();
 }
